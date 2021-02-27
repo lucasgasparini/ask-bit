52ce691b310eab8f9b0c9ecc01788a46272472e0
Lucas <lucasgasparinibr@gmail.com>
2021-02-26 22:12:36 -0300

    feat: add basic functionality

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,43 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Image from "next/image";
+import { useState } from "react";
+import styles from "../styles/Home.module.scss";
 
 export default function Home() {
+  let [answer, setAnswer] = useState(2);
+
+  const answerQuestion = () => {
+    answer = Math.random() < 0.5;
+    console.log(answer);
+    if (answer) {
+      setAnswer(1);
+    } else {
+      setAnswer(0);
+    }
+  };
+
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      {answer !== 0 && answer !== 1 && (
+        <Image src="/bit.png" width={128} height={128} />
+      )}
+      {answer === 1 && (
+        <div className={styles.answerContainer}>
+          <Image src="/yes.png" width={128} height={128} />
+          <h1 className={styles.yesAnswer}>Yes!</h1>
         </div>
-      </main>
+      )}
+      {answer === 0 && (
+        <div className={styles.answerContainer}>
+          <Image src="/no.png" width={128} height={128} />
+          <h1 className={styles.noAnswer}>No!</h1>
+        </div>
+      )}
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      {answer !== 0 && answer !== 1 && (
+        <button className={styles.questionButton} onClick={answerQuestion}>
+          Think of a question and press this button to ask Bit
+        </button>
+      )}
     </div>
-  )
+  );
 }
